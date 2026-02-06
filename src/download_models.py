0b5e49fe5a91b55e1973bb68e763f3b4b07c705d
rubyrubyrockets <wmjjj7kcdp@privaterelay.appleid.com>
2026-02-07 00:39:29 +0300

    Create download_models.py

---
+++ src/download_models.py
@@ -0,0 +1,2 @@
+ONNX_URL = "https://github.com/rubyrubyrockets/drummers-cue/releases/download/models/ru_RU-irina-medium.onnx"
+JSON_URL = "https://github.com/rubyrubyrockets/drummers-cue/releases/download/models/ru_RU-irina-medium.json"
